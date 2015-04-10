80dbef45c887660f43bd33fbb347da77929093c9
Wade Stebbings <wadestebbings@gmail.com>
2015-04-09 23:15:02 -0500

    books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
+  get 'books/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
