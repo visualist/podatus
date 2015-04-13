d509b5493c0607a53ac5066e9ed28226f394bf72
Wade Stebbings <wadestebbings@gmail.com>
2015-04-13 16:15:24 -0500

    background text (module) holds static text

---
+++ app/models/background_text.rb
@@ -0,0 +1,80 @@
+module BackgroundText
+
+  Alice = <<-END
+    The long grass rustled at her feet as the White Rabbit hurried by--the
+    frightened Mouse splashed his way through the neighbouring pool--she
+    could hear the rattle of the teacups as the March Hare and his friends
+    shared their never-ending meal, and the shrill voice of the Queen
+    ordering off her unfortunate guests to execution--once more the pig-baby
+    was sneezing on the Duchess's knee, while plates and dishes crashed
+    around it--once more the shriek of the Gryphon, the squeaking of the
+    Lizard's slate-pencil, and the choking of the suppressed guinea-pigs,
+    filled the air, mixed up with the distant sobs of the miserable Mock
+    Turtle.
+  END
+
+  Frank = <<-END
+    By the quantity of provision which I had consumed, I should guess that
+    I had passed three weeks in this journey; and the continual protraction
+    of hope, returning back upon the heart, often wrung bitter drops of
+    despondency and grief from my eyes.  Despair had indeed almost secured
+    her prey, and I should soon have sunk beneath this misery.  Once, after
+    the poor animals that conveyed me had with incredible toil gained the
+    summit of a sloping ice mountain, and one, sinking under his fatigue,
+    died, I viewed the expanse before me with anguish, when suddenly my eye
+    caught a dark speck upon the dusky plain.  I strained my sight to
+    discover what it could be and uttered a wild cry of ecstasy when I
+    distinguished a sledge and the distorted proportions of a well-known
+    form within.  Oh!  With what a burning gush did hope revisit my heart!
+    Warm tears filled my eyes, which I hastily wiped away, that they might
+    not intercept the view I had of the daemon; but still my sight was
+    dimmed by the burning drops, until, giving way to the emotions that
+    oppressed me, I wept aloud.
+  END
+
+  TwoCities = <<-END
+    Dinner-time, and still no Hundreds of people. In the arrangements of
+    the little household, Miss Pross took charge of the lower regions, and
+    always acquitted herself marvellously. Her dinners, of a very modest
+    quality, were so well cooked and so well served, and so neat in their
+    contrivances, half English and half French, that nothing could be
+    better. Miss Pross's friendship being of the thoroughly practical
+    kind, she had ravaged Soho and the adjacent provinces, in search of
+    impoverished French, who, tempted by shillings and half-crowns, would
+    impart culinary mysteries to her. From these decayed sons and daughters
+    of Gaul, she had acquired such wonderful arts, that the woman and girl
+    who formed the staff of domestics regarded her as quite a Sorceress,
+    or Cinderella's Godmother: who would send out for a fowl, a rabbit,
+    a vegetable or two from the garden, and change them into anything she
+    pleased.
+  END
+
+  Pap1 = <<-END
+    His sisters were anxious for his having an estate of his own; but,
+    though he was now only established as a tenant, Miss Bingley was by no
+    means unwilling to preside at his table--nor was Mrs. Hurst, who had
+    married a man of more fashion than fortune, less disposed to consider
+    his house as her home when it suited her. Mr. Bingley had not been of
+    age two years, when he was tempted by an accidental recommendation
+    to look at Netherfield House. He did look at it, and into it for
+    half-an-hour--was pleased with the situation and the principal
+    rooms, satisfied with what the owner said in its praise, and took it
+    immediately.
+  END
+
+  Pap2 = <<-END
+    Good gracious! Lord bless me! only think! dear me! Mr. Darcy! Who would
+    have thought it! And is it really true? Oh! my sweetest Lizzy! how rich
+    and how great you will be! What pin-money, what jewels, what carriages
+    you will have! Jane's is nothing to it--nothing at all. I am so
+    pleased--so happy. Such a charming man!--so handsome! so tall!--Oh, my
+    dear Lizzy! pray apologise for my having disliked him so much before. I
+    hope he will overlook it. Dear, dear Lizzy. A house in town! Every thing
+    that is charming! Three daughters married! Ten thousand a year! Oh,
+    Lord! What will become of me. I shall go distracted.
+  END
+
+  All = [Alice, Frank, TwoCities, Pap1, Pap2]
+
+end
+
